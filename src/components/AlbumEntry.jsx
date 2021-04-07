edb263449c214faf543ccec9dc2acac5785b8cb1
Vincent Thiele <hey@thielegram.de>
2021-04-07 19:39:36 +0200

    ADD album entry

--- src/components/AlbumEntry.jsx
+++ src/components/AlbumEntry.jsx
@@ -3,14 +3,27 @@ import React, { Component } from 'react';
 class AlbumEntry extends Component {
 	render() {
 		const { name, artist, year, image, colorScheme } = this.props.data;
-
 		return (
 			<div className='results-entry results-album'>
-				<div>
+				<div className='album-info'>
 					<img className='album-image' src={image} />
-					<div className='album-name'>{name}</div>
+					<div className='album-text'>
+						<div>{name}</div>
+						<div>
+							{artist} ({year})
+						</div>
+					</div>
 				</div>
-				<div></div>
+				<div className='album-colors'>
+					{colorScheme.map(color => (
+						<div
+							style={{
+								flex: 1,
+								height: '10px',
+								backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+							}}></div>
+					))}
+				</div>
 			</div>
 		);
 	}
