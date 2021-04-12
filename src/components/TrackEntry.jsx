d8b8fbc1a29362a0b9711370ea2915bec43fe64a
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-04-12 23:36:47 +0200

    ADD track entry

--- src/components/TrackEntry.jsx
+++ src/components/TrackEntry.jsx
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
 
 class TrackEntry extends Component {
-	render() {
-		const {
-			albumTitle,
-			image,
-			artist,
-			title,
-			currentTrack,
-			previousTrack,
-			nextTrack,
-		} = this.props.data;
+    render() {
+        const {
+            albumTitle,
+            image,
+            artist,
+            year,
+            currentTrack,
+            previousTrack,
+            nextTrack,
+            colorScheme,
+        } = this.props.data;
 
-		return (
-			<div className='results-entry results-album'>
-				<div>
-					<img className='album-image' src={image} />
-					<div className='album-name'>{albumTitle}</div>
-				</div>
-				<div></div>
-			</div>
-		);
-	}
+        return (
+            <div className='results-entry results-album'>
+                <div className='album-info'>
+                    <img className='album-image' src={image} />
+                    <div className='album-text'>
+                        <div>{albumTitle}</div>
+                        <div>
+                            {artist} ({year})
+                        </div>
+                    </div>
+                    <div className='album-tracks'>
+                        <span>{`${previousTrack.trackNr} ${previousTrack.title}`}</span>
+                        <span>{`${currentTrack.trackNr} ${currentTrack.title}`}</span>
+                        <span>{`${nextTrack.trackNr} ${nextTrack.title}`}</span>
+                    </div>
+                </div>
+                <div className='album-colors'>
+                    {colorScheme.map(color => (
+                        <div
+                            style={{
+                                flex: 1,
+                                height: '10px',
+                                backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+                            }}
+                        ></div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default TrackEntry;
