f24432d546a9216271168fe98eaa06451df75d4e
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-04-15 20:19:56 +0200

    EDIT album entry style

--- src/App.jsx
+++ src/App.jsx
@@ -9,123 +9,123 @@ import './App.css';
 import 'bulma/css/bulma.css';
 
 class App extends Component {
-	state = {
-		artists: [],
-		albums: [],
-		tracks: [],
-		mode: 'albums',
-		query: '',
-	};
+    state = {
+        artists: [],
+        albums: [],
+        tracks: [],
+        mode: 'albums',
+        query: '',
+    };
 
-	handleSearch = () => {
-		const { query, mode } = this.state;
-		console.log(query);
+    handleSearch = () => {
+        const { query, mode } = this.state;
+        console.log(query);
 
-		axios.get(`http://localhost:3001/${mode}/${query}`).then(res => {
-			switch (mode) {
-				case 'albums':
-					console.log('albums', res);
-					this.setState({
-						albums: res.data,
-						artists: [],
-						tracks: [],
-					});
-					break;
-				case 'artists':
-					console.log('artists', res);
-					this.setState({
-						albums: [],
-						artists: res.data,
-						tracks: [],
-					});
-					break;
-				case 'tracks':
-					console.log('tracks', res);
-					this.setState({
-						albums: [],
-						artists: [],
-						tracks: res.data,
-					});
-					break;
-			}
-		});
-	};
+        axios.get(`http://localhost:3001/${mode}/${query}`).then(res => {
+            switch (mode) {
+                case 'albums':
+                    this.setState({
+                        albums: res.data,
+                        artists: [],
+                        tracks: [],
+                    });
+                    break;
+                case 'artists':
+                    this.setState({
+                        albums: [],
+                        artists: res.data,
+                        tracks: [],
+                    });
+                    break;
+                case 'tracks':
+                    this.setState({
+                        albums: [],
+                        artists: [],
+                        tracks: res.data,
+                    });
+                    break;
+                default:
+                    this.setState({
+                        albums: [],
+                        artists: [],
+                        tracks: [],
+                    });
+            }
+        });
+    };
 
-	handleChange = event => {
-		this.setState({ query: event.target.value });
-	};
+    handleChange = event => {
+        this.setState({ query: event.target.value });
+    };
 
-	handleModeSelect = event => {
-		this.setState({ mode: event.target.value });
-	};
+    handleModeSelect = event => {
+        this.setState({ mode: event.target.value });
+    };
 
-	render() {
-		const { mode, artists, albums, tracks } = this.state;
+    render() {
+        const { mode, artists, albums, tracks } = this.state;
 
-		return (
-			<>
-				<header>
-					<h1>ACE</h1>
-				</header>
-				<main>
-					<div className='field has-addons'>
-						<p className='control'>
-							<span className='select'>
-								<select onChange={this.handleModeSelect}>
-									<option value='albums' defaultValue>
-										Albums
-									</option>
-									<option value='artists'>
-										Artists
-									</option>
-									<option value='tracks'>Tracks</option>
-								</select>
-							</span>
-						</p>
-						<p className='control'>
-							<input
-								className='input'
-								type='text'
-								onChange={this.handleChange}
-								placeholder={`Title of ${mode}`}
-							/>
-						</p>
-						<p className='control'>
-							<button
-								className='button'
-								onClick={this.handleSearch}>
-								Go
-							</button>
-						</p>
-					</div>
-					<div className='results'>
-						{albums.map((result, i) => {
-							return (
-								<AlbumEntry
-									key={i}
-									data={result}></AlbumEntry>
-							);
-						})}
-						{artists.map((result, i) => {
-							return (
-								<ArtistEntry
-									key={i}
-									data={result}></ArtistEntry>
-							);
-						})}
-						{tracks.map((result, i) => {
-							return (
-								<TrackEntry
-									key={i}
-									data={result}></TrackEntry>
-							);
-						})}
-					</div>
-				</main>
-				<footer></footer>
-			</>
-		);
-	}
+        return (
+            <>
+                <header className='header'>
+                    <h1 className='header-title'>ACE</h1>
+                    <p className='header-subtitle'>Album Color Extractor</p>
+                </header>
+                <main>
+                    <div className='field has-addons'>
+                        <p className='control'>
+                            <span className='select is-secondary'>
+                                <select onChange={this.handleModeSelect}>
+                                    <option value='albums' defaultValue>
+                                        Albums
+                                    </option>
+                                    <option value='artists'>Artists</option>
+                                    <option value='tracks'>Tracks</option>
+                                </select>
+                            </span>
+                        </p>
+                        <p className='control'>
+                            <input
+                                className='input'
+                                type='text'
+                                onChange={this.handleChange}
+                                placeholder={`Title of ${mode}`}
+                            />
+                        </p>
+                        <p className='control'>
+                            <button
+                                className='button'
+                                onClick={this.handleSearch}
+                            >
+                                Go
+                            </button>
+                        </p>
+                    </div>
+                    <div className='results'>
+                        {albums.map((result, i) => {
+                            return (
+                                <AlbumEntry key={i} data={result}></AlbumEntry>
+                            );
+                        })}
+                        {artists.map((result, i) => {
+                            return (
+                                <ArtistEntry
+                                    key={i}
+                                    data={result}
+                                ></ArtistEntry>
+                            );
+                        })}
+                        {tracks.map((result, i) => {
+                            return (
+                                <TrackEntry key={i} data={result}></TrackEntry>
+                            );
+                        })}
+                    </div>
+                </main>
+                <footer></footer>
+            </>
+        );
+    }
 }
 
 export default App;
--- src/components/AlbumEntry.jsx
+++ src/components/AlbumEntry.jsx
@@ -1,32 +1,72 @@
 import React, { Component } from 'react';
 
+const AlbumColorsMobile = ({ colors }) => (
+    <div className='album-colors-mobile'>
+        {colors.map(color => (
+            <div
+                style={{
+                    flex: 1,
+                    height: '10px',
+                    backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+                }}
+            ></div>
+        ))}
+    </div>
+);
+
 class AlbumEntry extends Component {
-	render() {
-		const { name, artist, year, image, colorScheme } = this.props.data;
-		return (
-			<div className='results-entry results-album'>
-				<div className='album-info'>
-					<img className='album-image' src={image} />
-					<div className='album-text'>
-						<div>{name}</div>
-						<div>
-							{artist} ({year})
+    render() {
+        const { name, artist, year, image, colorScheme } = this.props.data;
+        return (
+            <div className='album'>
+                <div className='album-info'>
+                    <img className='album-image' src={image} alt='' />
+                    <div className='album-details'>
+                        <p>{name}</p>
+                        <p>{artist}</p>
+                        <p>{year}</p>
+                    </div>
+                    {window.matchMedia('(min-width: 701px)').matches && (
+                        <div className='album-colors'>
+                            {colorScheme.map(color => (
+                                <div
+                                    style={{
+                                        flex: 1,
+                                        backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+                                    }}
+                                ></div>
+                            ))}
+                        </div>
+                    )}
+                    <button className='album-button'>Copy</button>
+                </div>
+                {window.matchMedia('(max-width: 700px)').matches && (
+                    <AlbumColorsMobile colors={colorScheme} />
+                )}
+            </div>
+            /* <div className='results-entry results-album'>
+					<div className='album-info'>
+						<img className='album-image' src={image} />
+						<div className='album-text'>
+							<div>{name}</div>
+							<div>
+								{artist} ({year})
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className='album-colors'>
-					{colorScheme.map(color => (
-						<div
-							style={{
-								flex: 1,
-								height: '10px',
-								backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
-							}}></div>
-					))}
-				</div>
-			</div>
-		);
-	}
+					<div className='album-colors'>
+						{colorScheme.map(color => (
+							<div
+								style={{
+									flex: 1,
+									height: '10px',
+									backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+								}}></div>
+						))}
+					</div>
+				</div> */
+        );
+    }
 }
 
 export default AlbumEntry;
