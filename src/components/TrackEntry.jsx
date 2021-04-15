4585a67ff43e8694a21ff8d353eb777a88c6d8a5
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-04-15 21:31:27 +0200

    EDIT track entry

--- src/components/TrackEntry.jsx
+++ src/components/TrackEntry.jsx
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import { Download } from 'react-feather';
+
+const ColorList = ({ colors, mobile }) => (
+    <div className={`album-colors${mobile ? '-mobile' : ''}`}>
+        {colors.map(color => (
+            <div
+                style={{
+                    flex: 1,
+                    height: mobile ? '10px' : '100%',
+                    backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+                }}
+            ></div>
+        ))}
+    </div>
+);
 
 class TrackEntry extends Component {
     render() {
@@ -14,7 +29,41 @@ class TrackEntry extends Component {
         } = this.props.data;
 
         return (
-            <div className='results-entry results-album'>
+            <div className='album'>
+                <div className='album-info track-info'>
+                    <img className='album-image' src={image} alt='' />
+                    <div className='album-details'>
+                        <p>{albumTitle}</p>
+                        <p>{artist}</p>
+                        <p>{year}</p>
+                    </div>
+                    <div className='album-tracks'>
+                        <p>
+                            {previousTrack.title
+                                ? `${previousTrack.trackNr}. ${previousTrack.title}`
+                                : ''}
+                            &nbsp;
+                        </p>
+                        <p>{`${currentTrack.trackNr}. ${currentTrack.title}`}</p>
+                        <p>
+                            {nextTrack.title
+                                ? `${nextTrack.trackNr}. ${nextTrack.title}`
+                                : ''}
+                            &nbsp;
+                        </p>
+                    </div>
+                    {window.matchMedia('(min-width: 701px)').matches && (
+                        <ColorList colors={colorScheme} />
+                    )}
+                    <button className='copy-button'>
+                        <Download />
+                    </button>
+                </div>
+                {window.matchMedia('(max-width: 700px)').matches && (
+                    <ColorList colors={colorScheme} mobile />
+                )}
+            </div>
+            /* <div className='results-entry results-album'>
                 <div className='album-info'>
                     <img className='album-image' src={image} />
                     <div className='album-text'>
@@ -40,7 +89,7 @@ class TrackEntry extends Component {
                         ></div>
                     ))}
                 </div>
-            </div>
+            </div> */
         );
     }
 }
