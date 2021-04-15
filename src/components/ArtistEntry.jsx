e003250345eebd4cb80ac5bc57beabc252782fad
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-04-15 21:11:00 +0200

    EDIT artist entry

--- src/components/ArtistEntry.jsx
+++ src/components/ArtistEntry.jsx
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 
 class ArtistEntry extends Component {
-	render() {
-		const { name, image, albums } = this.props.data;
+    render() {
+        const { name, image, albums } = this.props.data;
 
-		return (
-			<div className='results-entry results-artist'>
-				<img className='artist-image' src={image} />
-				<div className='artist-name'>{name}</div>
-				<div className='artist-albums'></div>
-			</div>
-		);
-	}
+        return (
+            <div className='artist'>
+                {image ? (
+                    <img className='artist-image' src={image} alt='Artist' />
+                ) : (
+                    <div
+                        style={{
+                            height: '8vh',
+                            width: '8vh',
+                            backgroundColor: '#dddddd',
+                            borderRadius: '4px',
+                        }}
+                    ></div>
+                )}
+                <div className='artist-name'>
+                    <p>{name}</p>
+                </div>
+                <div className='artist-albums'>
+                    {window.matchMedia('(max-width: 700px)').matches
+                        ? albums
+                              .slice(0, 2)
+                              .map(album => (
+                                  <img src={album} alt='Album cover' />
+                              ))
+                        : albums
+                              .slice(0, 4)
+                              .map(album => (
+                                  <img src={album} alt='Album cover' />
+                              ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ArtistEntry;
